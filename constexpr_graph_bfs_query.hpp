3fdf3046d82f7d9643e528d93056c6c2e9447218
Yiftah Waisman <yiftahw1@gmail.com>
2025-10-31 07:03:05 +0200

    transition to using a concept instead of an actual struct, compile time route check helper

--- constexpr_graph_bfs_query.hpp
+++ constexpr_graph_bfs_query.hpp
@@ -3,15 +3,18 @@
 #include <array>
 #include <algorithm>
 #include <cstddef>
-#include <cstdio>
+#include <concepts>
 #include <span>
 
-template <typename Node, typename Meta>
-struct Edge {
-    Node src;
-    Node dst;
-    Meta meta;
+template <typename E>
+concept EdgeConcept = requires(E e) {
+    { e.src };
+    { e.dst };
+    requires std::same_as<std::remove_cvref_t<decltype(e.src)>, std::remove_cvref_t<decltype(e.dst)>>;
 };
+
+template <typename E>
+using NodeType = std::remove_cvref_t<decltype(E::src)>;
 
 // ---------- constexpr queue ----------
 template <typename T, size_t Capacity>
@@ -34,21 +37,23 @@ struct ConstexprQueue {
 };
 
 // ---------- adjacency builder ----------
-template <typename Node, typename Meta, size_t NumEdges, size_t NodeCount>
-constexpr auto adjacency_list(const std::array<Edge<Node, Meta>, NumEdges>& edges) {
+template <typename EdgeType, size_t NumEdges, size_t NodeCount>
+    requires EdgeConcept<EdgeType>
+constexpr auto adjacency_list(const std::array<EdgeType, NumEdges>& edges) {
     std::array<std::array<size_t, NumEdges>, NodeCount> out{};
     std::array<size_t, NodeCount> counts{};
     for (size_t i = 0; i < NumEdges; ++i) {
-        Node u = edges[i].src;
+        NodeType<EdgeType> u = edges[i].src;
         out[u][counts[u]++] = i;
     }
     return std::make_pair(out, counts);
 }
 
 // ---------- result ----------
-template <typename Node, typename Meta, size_t MaxEdges>
+template <typename EdgeType, size_t MaxEdges>
+    requires EdgeConcept<EdgeType>
 struct BFSResult {
-    std::array<Edge<Node, Meta>, MaxEdges> path{};
+    std::array<EdgeType, MaxEdges> path{};
     size_t length = 0;
     bool found = false;
 
@@ -57,11 +62,26 @@ struct BFSResult {
     }
 };
 
+// ---------- comparison operator ----------
+template <typename EdgeType, size_t MaxEdges, size_t RouteLength>
+requires EdgeConcept<EdgeType>
+constexpr bool compare_routes(const BFSResult<EdgeType, MaxEdges>& a,
+                          const std::array<NodeType<EdgeType>, RouteLength>& b) {
+    if (a.length != RouteLength - 1) return false;
+    for (size_t i = 0; i < a.length; ++i) {
+        if (a.path[i].src != b[i] || a.path[i].dst != b[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // ---------- constexpr BFS ----------
-template <typename Node, typename Meta, size_t NumNodes, size_t NumEdges>
-constexpr BFSResult<Node, Meta, NumNodes - 1>
-bfs_edges(const std::array<Edge<Node, Meta>, NumEdges>& edges, Node start, Node goal) {
-    auto [adj, counts] = adjacency_list<Node, Meta, NumEdges, NumNodes>(edges);
+template <size_t NumNodes, typename EdgeType, size_t NumEdges>
+    requires EdgeConcept<EdgeType>
+constexpr BFSResult<EdgeType, NumNodes - 1>
+bfs_edges(const std::array<EdgeType, NumEdges>& edges, NodeType<EdgeType> start, NodeType<EdgeType> goal) {
+    auto [adj, counts] = adjacency_list<EdgeType, NumEdges, NumNodes>(edges);
     std::array<int, NumNodes> prev{};
     std::array<int, NumNodes> via_edge{};
     std::array<bool, NumNodes> visited{};
@@ -72,16 +92,16 @@ bfs_edges(const std::array<Edge<Node, Meta>, NumEdges>& edges, Node start, Node 
         visited[i] = false;
     }
 
-    ConstexprQueue<Node, NumNodes> q{};
+    ConstexprQueue<NodeType<EdgeType>, NumNodes> q{};
     q.push(start);
     visited[start] = true;
 
     while (!q.empty()) {
-        Node u = q.pop();
+        NodeType<EdgeType> u = q.pop();
         if (u == goal) break;
         for (size_t i = 0; i < counts[u]; ++i) {
             size_t eidx = adj[u][i];
-            Node v = edges[eidx].dst;
+            NodeType<EdgeType> v = edges[eidx].dst;
             if (!visited[v]) {
                 visited[v] = true;
                 prev[v] = static_cast<int>(u);
@@ -91,7 +111,7 @@ bfs_edges(const std::array<Edge<Node, Meta>, NumEdges>& edges, Node start, Node 
         }
     }
 
-    BFSResult<Node, Meta, NumNodes - 1> result{};
+    BFSResult<EdgeType, NumNodes - 1> result{};
     if (!visited[goal]) return result;
 
     // reconstruct path (edges)
--- tests/test_constexpr_graph_bfs_route_finder.cpp
+++ tests/test_constexpr_graph_bfs_route_finder.cpp
@@ -5,24 +5,26 @@
 enum Node { A, B, C, D, E, F, G };
 static constexpr size_t NodeCount = G + 1;
 
-struct EdgeData {};
+struct Edge {
+    Node src;
+    Node dst;
+};
+static_assert(EdgeConcept<Edge>);
 
-constexpr std::array<Edge<Node, EdgeData>, 6> edges = {{
-    {A, B, {}},
-    {B, C, {}},
-    {C, D, {}},
-    {C, E, {}},
-    {C, F, {}},
-    {B, F, {}}
+constexpr std::array<Edge, 6> edges = {{
+    {A, B},
+    {B, C},
+    {C, D},
+    {C, E},
+    {C, F},
+    {B, F}
 }};
 
 // ---------- compile time test ----------
 
 TEST(ConstexprBFS, RouteAtoF) {
-    constexpr auto route_A_to_F = bfs_edges<Node, EdgeData, NodeCount>(edges, A, F);
-
+    constexpr auto route_A_to_F = bfs_edges<NodeCount>(edges, A, F);
     static_assert(route_A_to_F.found);
     static_assert(route_A_to_F.length == 2);
-    static_assert(route_A_to_F.path[0].src == A);
-    static_assert(route_A_to_F.path[1].dst == F);
+    static_assert(compare_routes(route_A_to_F, std::array{A, B, F})); // 3 nodes are covered in 2 edges
 }
