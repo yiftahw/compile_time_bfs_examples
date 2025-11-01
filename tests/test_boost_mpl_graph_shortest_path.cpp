406191efdea1b581298a5599e01c0c0ee81b8049
Yiftah Waisman <yiftahw1@gmail.com>
2025-11-01 19:40:23 +0200

    failing test case that should pass

--- tests/test_boost_mpl_graph_shortest_path.cpp
+++ tests/test_boost_mpl_graph_shortest_path.cpp
@@ -23,14 +23,20 @@ struct D{};
 struct E{}; 
 struct F{}; 
 struct G{};
+struct H{};
 
 // edges
-struct A_B{}; 
+struct A_B{};
+struct A_C{};
 struct B_C{}; 
+struct B_D{};
+struct B_F{};
 struct C_D{}; 
 struct C_E{}; 
-struct C_F{}; 
-struct B_F{};
+struct C_F{};
+struct D_F{};
+struct F_H{};
+struct E_G{};
 
 // define the graph structure
 typedef mpl::vector<
@@ -41,8 +47,9 @@ typedef mpl::vector<
     mpl::vector<C_F, C, F>,
     mpl::vector<B_F, B, F>
 > my_graph_data_type;
-
 typedef mpl_graph::incidence_list_graph<my_graph_data_type> my_graph_type;
+
+#define EDGE(src,dst) mpl::vector<src##_##dst, src, dst>
 
 // Test that a route exists from A to F and is the shortest path A -> B -> F
 TEST(BFSRouteFinderTest, RouteFromAToFExists) {
@@ -56,7 +63,7 @@ TEST(BFSRouteFinderTest, RouteFromAToFExists) {
     static_assert(path_size == 3);
 
     // Check that the route is actually A -> B -> F
-    static constexpr bool path_correct = mpl::equal<bfs_route_path_t<route_A_to_F>, mpl::vector3<A,B,F>>::value;
+    static constexpr bool path_correct = mpl::equal<bfs_route_path_t<route_A_to_F>, mpl::vector<A,B,F>>::value;
     static_assert(path_correct);
 }
 
@@ -65,4 +72,22 @@ TEST(BFSRouteFinderTest, NoRouteFromAToG) {
     using route_A_to_G = bfs_route_query_result_t<my_graph_type, A, G>;
     // Check that route was not found
     static_assert(!bfs_route_found_v<route_A_to_G>);
+}
+
+TEST(BFSRouteFinderTest, Test2) {
+    using graph_2_edges_t = mpl::vector<
+        EDGE(A,B),
+        EDGE(B,D),
+        EDGE(D,F),
+        EDGE(F,H),
+        EDGE(A,C),
+        EDGE(C,E),
+        EDGE(E,G)
+    >;
+    using graph_2_t = mpl_graph::incidence_list_graph<graph_2_edges_t>;
+
+    using route_A_to_H = bfs_route_query_result_t<graph_2_t, A, H>;
+    static_assert(bfs_route_found_v<route_A_to_H>);
+    using route = bfs_route_path_t<route_A_to_H>;
+    static_assert(mpl::equal<route, mpl::vector<A,B,D,F,H>>::value);
 }
